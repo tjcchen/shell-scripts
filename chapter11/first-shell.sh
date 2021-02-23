a6723a3f70e01904cd03e85d1f6b755e449ea250
Andy Chen <chenyangpoppin@hotmail.com>
2021-02-23 17:49:40 +0800

    docs: comment thing

--- chapter11/first-shell.sh
+++ chapter11/first-shell.sh
@@ -1,4 +1,8 @@
-# !/bin/bash
-# This script displays the date and who's logged on
+#!/bin/bash
+# This script displays the date and who's logged on.
+
+# How to execute this bash file:
+# 1. chmod u+x first-shell.sh
+# 2. ./first-shell.sh
 date
 who
