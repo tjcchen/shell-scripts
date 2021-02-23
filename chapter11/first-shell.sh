901dae4064c6a718387dea58cd8fc2807dd0f21f
Andy Chen <chenyangpoppin@hotmail.com>
2021-02-23 18:03:49 +0800

    feat: add echo command usage

--- chapter11/first-shell.sh
+++ chapter11/first-shell.sh
@@ -4,5 +4,10 @@
 # How to execute this bash file:
 # 1. chmod u+x first-shell.sh
 # 2. ./first-shell.sh
+
+# -n flag would display message and date on the same line
+echo -n "The time and date are: "
 date
+
+echo "Let's see who's logged into the system: "
 who
