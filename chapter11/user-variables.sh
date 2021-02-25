96b1ff94e7370da8d09e503798b9b3afd8825111
Andy Chen <chenyangpoppin@hotmail.com>
2021-02-25 22:47:59 +0800

    docs: update content

--- chapter11/user-variables.sh
+++ chapter11/user-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Note:
+# 1. user variables can be alphabets, numbers, and underscore combination, the length is no longer than 20 characters.
+# 2. user variables are case sensitive. var1 and Var1 are two different variables.
+# 3. assigning value to variable with "=", there is no space within the whole expression, like name="jackie".
+
 # testing variables
 days=10
 guest="katie"
@@ -8,3 +13,9 @@ echo "$guest checked in $days days ago"
 days=5
 guest="Jessica"
 echo "${guest} checked in ${days} days ago"
+
+# reassigning a variable value to another variable
+value1=10
+# please note this place
+value2=$value1
+echo "The resulting value $value2"
