04edfb31153c4821a2dea75734ec9ed9e7505924
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-05 22:20:44 +0800

    feat: add an example about command expr

--- chapter11/calculate.sh
+++ chapter11/calculate.sh
@@ -4,3 +4,12 @@ expr 1 + 5
 
 # please note this place, utilize \ to handle multiply
 expr 3 \* 6
+
+########################
+## calculate quotient
+########################
+var1=10
+var2=20
+var3=$(expr $var2 / $var1)
+echo The result is $var3
+echo "The result is ${var3}"
