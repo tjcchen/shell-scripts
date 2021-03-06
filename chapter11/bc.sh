86144388349b29bc7b934488f57fe645caa2e794
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-06 23:56:18 +0800

    feat: add bc calculation examples

--- chapter11/bc.sh
+++ chapter11/bc.sh
@@ -5,3 +5,16 @@
 # The answer is .6880
 var1=$(echo "scale=4; 3.44 / 5" | bc)
 echo "The answer is ${var1}"
+
+# simple example 1
+# The answer for this is 2.2222
+var11=100
+var12=45
+var13=$(echo "scale=4; $var11 / $var12" | bc)
+echo The answer for this is $var13
+
+# simple example 2
+# utilize above variable
+# The final result is 99.9990( 2.2222 * 45 = 99.999 )
+var14=$(echo "scale=4; $var13 * $var12" | bc)
+echo The final result is $var14
