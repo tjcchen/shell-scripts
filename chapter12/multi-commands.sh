6599ee0e6cf9bd616eacc026f3c94b59086dee71
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-10 23:49:58 +0800

    feat: testing multiple commands in one if statement

---
+++ chapter12/multi-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# testing multiple commands in the then section
+#
+testuser=chen
+#
+
+# TODO: test this part
+if grep $testuser /etc/passwd
+then
+  echo "This is my first command"
+  echo "This is my second command"
+  echo "I can even put in other commands besides echo:"
+  ls -a /home/$testuser/.b*
+fi
