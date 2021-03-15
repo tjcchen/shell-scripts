ec1e0b046c05cc7c97ad16d8972ee1c00ba267f3
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-15 22:42:44 +0800

    feat: test string inequality

--- chapter12/test-string-comparison.sh
+++ chapter12/test-string-comparison.sh
@@ -9,3 +9,13 @@ then
 else
   echo "Welcome user: $USER!"
 fi
+
+# eg2: test string inequality
+testuser=baduser
+
+if [ $USER != testuser ]
+then
+  echo "This is not $testuser"
+else
+  echo "Welcome $testuser!"
+fi
