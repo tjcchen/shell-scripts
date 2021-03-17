830cdb1a03063c0293d756c64fe8625a21ba1605
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-17 22:23:49 +0800

    feat: add string comparison example

--- chapter12/test-string-comparison.sh
+++ chapter12/test-string-comparison.sh
@@ -19,3 +19,15 @@ then
 else
   echo "Welcome $testuser!"
 fi
+
+# eg3: test string greater than(>) or less than(<), note \ character is important in comparsion
+# print out: basketball is greater than hockey
+var1=basketball
+var2=hockey
+
+if [ $var1 \> $var2 ]
+then
+  echo "$var1 is greater than $var2"
+else
+  echo "$var1 is less than $var2"
+fi
