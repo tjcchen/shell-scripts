f8fa58d0060c3f8c171943a0a9d9bb72a39269a0
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-19 22:44:47 +0800

    feat: check directory existance

---
+++ chapter12/test-file-comparison.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Look before you leap
+
+# file comparison:
+# -d directory existance
+
+jump_directory=/usr/local
+
+if [ -d $jump_directory ]
+then
+  echo "The $jump_directory directory exists"
+  cd $jump_directory
+  ls
+else
+  echo "The $jump_directory directory does not exist"
+fi
