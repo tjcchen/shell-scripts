2b1fee1b7f980b16949f6af89de701b17ce5c265
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-24 23:27:30 +0800

    feat: add check file existance script

---
+++ chapter12/check-file-existance2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Check if either a directory or file exists
+#
+
+item_name=$HOME
+echo
+echo "The item being checked: $item_name"
+echo
+
+if [ -e $item_name ]
+then
+  echo "Yes, $item_name, does exist."
+  echo "But is it a file?"
+
+  if [ -f $item_name ]
+  then
+    echo "Yes, $item_name is a file."
+  else
+    echo "No, $item_name is not a file"
+  fi
+else
+  echo "The item, $item_name, does not exist."
+  echo "Nothing to update"
+fi
