dfcaf1db2b4d8324e2534aa01a7100f154858dde
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-27 23:02:29 +0800

    fix: fix permission issue

--- chapter12/check-file-permission.sh
+++ chapter12/check-file-permission.sh
@@ -1,1 +1,21 @@
-#!bin/bash# check file permission, testing if you can read a file## pwfile=/etc/shadowpwfile=/Users/chen/shell-scripts/chapter12/testfile# -e exist, -f file, -d directory, -r readable# first, test if the file exists, and is a fileif [ -f $pwfile ]then  # now test if you can read it  if [ -r $pwfile ]  then    tail $pwfile  else    echo "Sorry, I am unable to read the $pwfile file"  fielse  echo "Sorry, the file $pwfile does not exist"fi
+#!/bin/bash
+# check file permission, testing if you can read a file
+#
+
+# pwfile=/etc/shadow
+pwfile=/Users/chen/shell-scripts/chapter12/testfile
+
+# -e exist, -f file, -d directory, -r readable
+# first, test if the file exists, and is a file
+if [ -f $pwfile ]
+then
+  # now test if you can read it
+  if [ -r $pwfile ]
+  then
+    tail $pwfile
+  else
+    echo "Sorry, I am unable to read the $pwfile file"
+  fi
+else
+  echo "Sorry, the file $pwfile does not exist"
+fi
