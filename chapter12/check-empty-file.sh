28cb827b1798dfbec11d1712edd4948dd538c457
Andy Chen <chenyangpoppin@hotmail.com>
2021-03-28 21:48:08 +0800

    docs: comment thing

--- chapter12/check-empty-file.sh
+++ chapter12/check-empty-file.sh
@@ -4,17 +4,17 @@
 
 file_name=$HOME/sentinel
 
-if [ -f $file_name ]
+if [ -f $file_name ]   # file exist
 then
-  if [ -s $file_name ]
+  if [ -s $file_name ] # file exist, and not empty
   then
     echo "The $file_name file exists and has data in it."
     echo "Will not remove this file."
-  else
+  else                 # file exist, but empty
     echo "The $file_name file exists, but is empty."
     echo "Deleting empty file..."
     rm $file_name
   fi
-else
+else                   # file not exist
   echo "File, $file_name, does not exist."
 fi
