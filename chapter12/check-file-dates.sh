1cea079ffe10cb3a9ed145d53d61332b4707c866
Andy Chen <chenyangpoppin@hotmail.com>
2021-04-06 08:24:03 +0800

    feat: check file dates

---
+++ chapter12/check-file-dates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# file dates comparison
+#
+# syntax:
+# -nt: file A is newer than file B
+# -ot: file A is older than file B
+#
+
+# print out:
+# The check-file-dates.sh file is newer than check-file-execution.sh
+# The check-file-execution.sh is older than the check-file-dates.sh file
+#
+
+if [ check-file-dates.sh -nt check-file-execution.sh ]
+then
+  echo "The check-file-dates.sh file is newer than check-file-execution.sh"
+else
+  echo "The check-file-execution.sh file is newer than check-file-dates.sh"
+fi
+
+if [ check-file-execution.sh -ot check-file-dates.sh ]
+then
+  echo "The check-file-execution.sh is older than the check-file-dates.sh file"
+fi
