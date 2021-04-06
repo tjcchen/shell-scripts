fcdc6c25191df7d3902b1267736c56562d6f538a
Andy Chen <chenyangpoppin@hotmail.com>
2021-04-06 08:30:33 +0800

    docs: comment thing

--- chapter12/check-file-dates.sh
+++ chapter12/check-file-dates.sh
@@ -11,6 +11,10 @@
 # The check-file-execution.sh is older than the check-file-dates.sh file
 #
 
+# note: before we use '-nt' or '-ot', we must check file existance first,
+# the command won't do that kind of thing
+#
+
 if [ check-file-dates.sh -nt check-file-execution.sh ]
 then
   echo "The check-file-dates.sh file is newer than check-file-execution.sh"
