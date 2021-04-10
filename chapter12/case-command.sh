3a7e0c59b318a14756c2e24e557607a4267a0d4c
Andy Chen <chenyangpoppin@hotmail.com>
2021-04-10 23:26:52 +0800

    feat: add case command usage

---
+++ chapter12/case-command.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# looking for a possible value
+# if-then-else and case
+#
+
+# print out: Sorry, you are not allowed here
+if [ $USER = "rich" ]
+then
+  echo "Welcome $USER"
+  echo "Please enjoy your visit"
+elif [ $USER = "barbara" ]
+then
+  echo "Welcome $USER"
+  echo "Please enjoy your visit"
+elif [ $USER = "testing" ]
+then
+  echo "Special testing account"
+elif [ $USER = "jessica" ]
+then
+  echo "Do not forget to logout when you're done"
+else
+  echo "Sorry, you are not allowed here"
+fi  
